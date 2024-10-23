71bfd85d3cfb5be6b064ebbc71f0cf97bfef4e64
penbolt1220 <penbolt1220@gmail.com>
2024-10-24 00:37:41 +0900

    'KMeansクラスを使ったクラスタリング'

--- 9-2.py
+++ 9-2.py
@@ -17,3 +17,26 @@ import seaborn as sns
 
 import sklearn
 
+#k-meansを使うためのインポート
+from sklearn.cluster import KMeans
+#データ取得のためのインポート
+from sklearn.datasets import make_blobs
+
+#サンプルデータの生成
+#注意：make＿blobsは二つの値を返すため、一方は使用しない「＿」で受け取る
+X,_ = make_blobs(random_state=10)
+
+plt.title('9-2-2-1')
+plt.scatter(X[:,0], X[:,1], color = 'black')
+
+#KMeansクラスの初期化
+kmeans = KMeans(init = 'random', n_clusters=3)
+
+#クラスターの重心を計算
+kmeans.fit(X)
+
+#クラスター番号を予測
+y_pred = kmeans.predict(X)
+
+plt.title('9-2-2-2')
+plt.scatter(X[:,0], X[:,1], color = 'black')
