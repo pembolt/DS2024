c00dbb97d33fbc0b6684957350e00a3ff4b04a92
penbolt1220 <penbolt1220@gmail.com>
2024-10-24 00:59:00 +0900

    '9-2-2-3結果の表示'

--- 9-2.py
+++ 9-2.py
@@ -38,5 +38,15 @@ kmeans.fit(X)
 #クラスター番号を予測
 y_pred = kmeans.predict(X)
 
-plt.title('9-2-2-2')
-plt.scatter(X[:,0], X[:,1], color = 'black')
+#concatでデータを横に結合(axis = 1を指定)
+merge_data = pd.concat([pd.DataFrame(X[:,0]), pd.DataFrame(X[:,1]), pd.DataFrame(y_pred)], axis=1)
+
+#上記のデータについて、X軸をfeaturel,Y軸をfeature2,クラスター番号をclusterと列名指定
+merge_data.columns = ['feature1', 'feature2', 'cluster']
+
+#クラスタリング結果をグラフ化
+ax = None
+colors = ['blue', 'red', 'green']
+for i, data in merge_data.groupby('cluster'):
+    ax = data.plot.scatter(x = 'feature1', y = 'feature2', color = colors[i],
+                                             label = f'cluster{i}', ax = ax)
