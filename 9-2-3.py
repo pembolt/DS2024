de88b55eff62f7ae517ae74d41819402dea98f0a
penbolt1220 <penbolt1220@gmail.com>
2024-10-24 14:13:18 +0900

    '9-2-3-2データの読み込み'

---
+++ 9-2-3.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 24 13:31:55 2024
+
+@author: toshiki
+"""
+import numpy as np
+import numpy.random as random
+import scipy as sp
+from pandas import Series, DataFrame
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import seaborn as sns
+
+import sklearn
+
+#k-meansを使うためのインポート
+from sklearn.cluster import KMeans
+#データ取得のためのインポート
+from sklearn.datasets import make_blobs
+
+#webからデータを取得したリ、zipファイルを扱うためのライブラリ
+import requests, zipfile
+import io
+
+zip_file_url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/00222/bank.zip'
+
+r = requests.get(zip_file_url, stream = True)
+z = zipfile.ZipFile(io.BytesIO(r.content))
+z.extractall()
+
+#対象データの読み込み
+bank = pd.read_csv('bank-full.csv', sep = ';')
+bank.head()
+
